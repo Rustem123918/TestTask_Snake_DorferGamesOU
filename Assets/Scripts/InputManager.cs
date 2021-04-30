242d0d000752145ff5dda4e3852f3c125b803803
Rustem Saifullin <rustem123918@mail.ru>
2021-04-30 18:11:47 +0300

    Реализовано управление для мобилок

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -46,6 +46,32 @@ public class InputManager : Singleton<InputManager>
     }
     private void MobileInput()
     {
+        if (Input.touches.Length > 0)
+        {
+            var touch = Input.touches[0];
+            var ray = Camera.main.ScreenPointToRay(touch.position);
 
+            if (touch.phase == TouchPhase.Began)
+            {
+                RaycastHit raycastHit;
+                if(Physics.Raycast(ray, out raycastHit))
+                {
+                    var currentX = _snake.transform.position.x;
+                    var targetX = raycastHit.point.x;
+                    var translation = targetX - currentX;
+                    _snake.Move(new Vector3(translation, 0, 0));
+                }
+            }
+        }
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    var mousePos = Input.mousePosition;
+        //    Debug.Log(mousePos);
+        //    var ray = Camera.main.ScreenPointToRay(mousePos);
+        //    RaycastHit raycastHit;
+        //    if(Physics.Raycast(ray, out raycastHit))
+        //        Debug.Log(raycastHit.point);
+            
+        //}
     }
 }
--- Assets/Scripts/SnakeHead.cs
+++ Assets/Scripts/SnakeHead.cs
@@ -1,17 +1,22 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SnakeHead : MonoBehaviour
 {
-    public Snake snake;
+    private Snake _snake;
+    private void Start()
+    {
+        _snake = GetComponentInParent<Snake>();
+    }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Human"))
+        if (other.CompareTag("Human") || other.CompareTag("Crystal"))
         {
             Destroy(other.gameObject);
         }
-        else if (other.CompareTag("Bad Thing"))
+        else if (other.CompareTag("Bad Thing") || other.CompareTag("Bad Human"))
         {
-            snake.transform.position = Vector3.zero;
+            SceneManager.LoadScene("Main");
         }
     }
 }
